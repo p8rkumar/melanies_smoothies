f51a04b27c8da337fcf635dfabc20fb684776d99
Prashant Kumar <65176356+p8rkumar@users.noreply.github.com>
2026-01-12 15:24:09 +0530

    Revise title and add smoothie customization prompt
    Updated title and added a description for smoothie customization.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-#st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.title('My Parents New Healthy Diner')
-st.write("""Breakfast Menu""")
-
+st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+#st.title('My Parents New Healthy Diner')
+#st.write("""Breakfast Menu""")
+st.write("""Choose the fruits you want in a custom Smoothie!""")
 name_of_order = st.text_input('Name on Smoothie:')
 st.write("The Name on your Smoothie will be: ", name_of_order)
 
