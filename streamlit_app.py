b37e6042655df11b62482f9fbe41f3cbefa295e6
Prashant Kumar <65176356+p8rkumar@users.noreply.github.com>
2026-01-12 15:20:33 +0530

    Update title and description for smoothie app
    Update title and description for smoothie app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,9 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write("""Choose the fruits you want in the custom Smoothie!""")
+#st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.title('My Parents New Healthy Diner')
+st.write("""Breakfast Menu""")
 
 name_of_order = st.text_input('Name on Smoothie:')
 st.write("The Name on your Smoothie will be: ", name_of_order)
